12db0cd6acf801819d9a1a07d64711de7d445710
wmknudson <59666519+wmknudson9@users.noreply.github.com>
2020-05-29 23:15:02 -0700

    Take Over TestGame1 with start of SkateGame
    Currently you can jump with Space until the road scrolls out from under SkateDuck.

---
+++ Assets/Scripts/SkateGame/SkatePlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkatePlayer : MonoBehaviour
+{
+    public Vector2 jumpForce = new Vector2(0, 200);
+    public bool isGrounded = false;
+    Rigidbody2D jumpBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            jumpBody.AddForce(jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
