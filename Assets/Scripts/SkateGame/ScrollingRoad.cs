0c5fb576f4ab6a4244fc5e1bd44937b31a741f6e
wmknudson <59666519+wmknudson9@users.noreply.github.com>
2020-06-02 23:21:31 -0700

    Touch up SkateGame
    New assets, new obstacle setups, new obstacles.

--- Assets/Scripts/SkateGame/ScrollingRoad.cs
+++ Assets/Scripts/SkateGame/ScrollingRoad.cs
@@ -26,18 +26,18 @@ public class ScrollingRoad : MonoBehaviour
     private void OnBecameInvisible()
     {
 
-
+        /*
         // Murder children
         for (int i = 0; i < spawnedObstacles.Count; ++i)
         {
             Destroy(spawnedObstacles[i]);
         }
         spawnedObstacles.Clear();
-
-        GameObject road_obstacle = Instantiate(obstacles[0]);
-        road_obstacle.transform.SetParent(this.transform);
-        road_obstacle.transform.localPosition = new Vector3(Random.Range(-1, 1), 0.4f, 0);
-        spawnedObstacles.Add(road_obstacle);
+        */
+        Instantiate(obstacles[Random.Range(0, obstacles.Count)]);
+        //road_obstacle.transform.SetParent(this.transform);
+        //road_obstacle.transform.localPosition = new Vector3(Random.Range(-1, 1), 0.4f, 0);
+        //spawnedObstacles.Add(road_obstacle);
 
         // Reset road after it goes offscreen
         transform.position = offScreenTeleportPosition;
---
+++ Assets/Scripts/SkateGame/SkateObstacle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkateObstacle : MonoBehaviour
+{
+    public Vector3 movementSpeed = new Vector3(-2f, 0);
+    public Vector3 offScreenTeleportPosition = new Vector3(13, -0.5f);
+    public int spawnDeviationMax = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move road
+        transform.position += movementSpeed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Death"))
+        {
+            int spawnDeviation = Random.Range(0, spawnDeviationMax);
+            int spawnOrDelete = Random.Range(1, 5);
+            if (spawnOrDelete == 1)
+            {
+                offScreenTeleportPosition.x += spawnDeviation;
+                transform.position = offScreenTeleportPosition;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
